e5fcddc011fda3c45fdd7ddae8da04826aa66143
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2024-12-30 02:09:24 -0600

    cracked two sum

---
+++ DSA Neetcode Roadmap/two-sum/two sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> myMap;
+        for (int i = 0; i < nums.size(); i++) {
+            // myMap[nums[i]] = i;
+            myMap.insert({nums[i], i});
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int difference = target - nums[i];
+            auto mapElement = myMap.find(difference);
+            if (mapElement != myMap.end() && (mapElement->second != i)) {
+                return {i, mapElement->second};
+            }
+        }
+        return {};
+    }
+};
