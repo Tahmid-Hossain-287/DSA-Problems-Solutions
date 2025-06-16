d9bfff2a3bd3b0457d64216fd41bac7f50f6d676
tahmid <mstahmid0@gmail.com>
2025-06-16 16:15:31 -0500

    finished the optimal solution for sudoku solver

--- DSA Neetcode Roadmap/Valid Sudoku/optimal-solution.py
+++ DSA Neetcode Roadmap/Valid Sudoku/optimal-solution.py
@@ -1,18 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = {}
-        column = {}
-        box = {}
+        row = defaultdict(set)
+        column = defaultdict(set)
+        box = defaultdict(set)
 
-        for r in board:
-            for c in r:
-                if (board[r][c] in row[r] or
-                board[r][c] in column[c] or
-                board[r][c] in box[(row //3, column //3)]):
-                    return False
-                
-                row[r].append(board[r][c])
-                column[c].append(board[r][c])
-                box[(row // 3, column // 3)].append(board[r][c])
+        for r in range(9):
+            for c in range(9):
+                if (board[r][c] != "."):
+                    if (board[r][c] in row[r] or
+                    board[r][c] in column[c] or
+                    board[r][c] in box[(r //3, c //3)]):
+                        return False
+                    
+                    row[r].add(board[r][c])
+                    column[c].add(board[r][c])
+                    box[(r // 3, c // 3)].add(board[r][c])
 
         return True
